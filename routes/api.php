6437ed6988354bd02babc60bc822b6d57322058f
vagrant <vagrant@homestead>
2018-12-07 14:44:31 +0000

    dingo api first commit

---
+++ routes/api.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+$api = app('Dingo\Api\Routing\Router');
+
+$api->version('v1', function ($api) {
+    $api->get('version', function () {
+        return response('this is version v1');
+    });
+    $api->get('exception', function () {
+        throw new Exception('this is exception');
+    });
+});
+
+
+$api->version('v2', function ($api) {
+    $api->get('version', function () {
+        return response('this is version v2');
+    });
+});
